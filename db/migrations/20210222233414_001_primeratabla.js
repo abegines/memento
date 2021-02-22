7ad6c2d1ea6fa840c8e08bfc91e66cafcaa868a9
Antonio Begines <antonio@airantonio.begines.net>
2021-02-23 00:30:48 +0100

    knex migrations

---
+++ db/migrations/20210222233414_001_primeratabla.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('usuario', function (table) {
+     table.increments('id').primary();
+     table.string('email', 255).notNullable();
+     table.string('nombre', 255).notNullable();
+     table.enu('nivelUsuario', ['I','U','C','T','A'] ).notNullable();
+     table.boolean('esActivo').notNullable();
+  })
+  .createTable('estado', function (table) {
+     table.string('codEstado',2).notNullable();
+     table.string('nomEstado',30).notNullable();
+     table.integer('prioridadEstado').notNullable();
+     table.string('iconoEstado', 30).notNullable();
+     table.boolean('esDisponible').notNullable();
+     table.primary('codEstado');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTable("usuario")
+  .dropTable("estado");
+};
---
+++ db/seeds/initload.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('estado').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('estado').insert([
+        {codEstado: 'A', nomEstado: 'Nuevo', prioridadEstado: 8000, iconoEstado: 'EstadoA.png', esDisponible: true},
+        {codEstado: 'B', nomEstado: 'Asignado', prioridadEstado: 7000, iconoEstado: 'EstadoB.png', esDisponible: true},
+        {codEstado: 'B2', nomEstado: 'Reasignado', prioridadEstado: 7000, iconoEstado: 'EstadoB.png', esDisponible: true},
+        {codEstado: 'X', nomEstado: 'Reabierto', prioridadEstado: 9000, iconoEstado: 'EstadoX.png', esDisponible: true},
+        {codEstado: 'Y', nomEstado: 'Terminado', prioridadEstado: 2000, iconoEstado: 'EstadoY.png', esDisponible: true},
+        {codEstado: 'Z', nomEstado: 'Cerrado', prioridadEstado: 1000, iconoEstado: 'EstadoZ.png', esDisponible: true},
+      ]);
+    });
+};
